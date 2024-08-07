ed2859fce94a852f567300f7bc8317998ab25817
Rehema Mwaka <mwakabayah@gmail.com>
2024-08-07 10:11:36 +0300

    Added has title test for Nayms dashboard

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto("https://app.testing2.naymsnext.com/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Opportunities | Nayms/);
 });
 
 test('get started link', async ({ page }) => {
