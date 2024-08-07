4557063e2fabf877d8092c75ec89ef7b9543dd98
Rehema Mwaka <mwakabayah@gmail.com>
2024-08-07 10:56:22 +0300

    Added my first test

---
+++ tests/first_test.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://app.testing2.naymsnext.com/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Nayms/);
+});
+
+test("Investments link", async ({ page }) => {
+  await page.goto("https://app.testing2.naymsnext.com/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Investments" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Portfolio Summary" })
+  ).toBeVisible();
+});
