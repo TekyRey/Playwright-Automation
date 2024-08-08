323fa766b46fc523d7a4e5b6a4dbf60bc63e7f3e
Rehema Mwaka <mwakabayah@gmail.com>
2024-08-08 09:41:16 +0300

    Used hooks and grouped tests

--- tests/first_test.spec.ts
+++ tests/first_test.spec.ts
@@ -1,20 +1,23 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://app.testing2.naymsnext.com/");
+test.describe("Nayms App Tests", () => {
+  // Hook to navigate to the page before each test
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://app.testing2.naymsnext.com/");
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Nayms/);
-});
+  test("has title", async ({ page }) => {
+    // Expect the title to contain "Nayms"
+    await expect(page).toHaveTitle(/Nayms/);
+  });
 
-test("Investments link", async ({ page }) => {
-  await page.goto("https://app.testing2.naymsnext.com/");
+  test("Investments link", async ({ page }) => {
+    // Click the Investments link
+    await page.getByRole("link", { name: "Investments" }).click();
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Investments" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Portfolio Summary" })
-  ).toBeVisible();
+    // Expect the Portfolio Summary heading to be visible
+    await expect(
+      page.getByRole("heading", { name: "Portfolio Summary" })
+    ).toBeVisible();
+  });
 });
